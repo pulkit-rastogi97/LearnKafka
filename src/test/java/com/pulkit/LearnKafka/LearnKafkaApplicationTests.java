8a422e058bb0b64f6c80d382dc66a1c8ba45058a
Pulkit Rastogi <pulkit.rastogi97@gmail.com>
2026-05-14 03:59:48 +0530

    Learn Kafka: Kafka Producer Setup

---
+++ src/test/java/com/pulkit/LearnKafka/LearnKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulkit.LearnKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
